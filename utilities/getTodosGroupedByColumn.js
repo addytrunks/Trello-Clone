9f3f935e69960c87bc99ecc55ff6a1562e9f4de7
Adhithya Srivatsan <addytrunks@gmail.com>
2023-06-28 17:14:08 +0530

    unwanted

--- utilities/getTodosGroupedByColumn.js
+++ utilities/getTodosGroupedByColumn.js
@@ -4,20 +4,6 @@ export const getTodosGroupedByColumn = async () => {
     const datas = await databases.listDocuments(process.env.NEXT_PUBLIC_DATABASE_ID,process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID)
     
     const todos = datas.documents;
-    const groupedTodosByColumn = {
-        "todo":[],
-        "inprogress":[],
-        "done":[]
-    }
-    
-    todos.forEach((todo) => {
-        groupedTodosByColumn[`${todo.status}`].push({
-            "id":todo.$id,
-            "createdAt":todo.$createdAt,
-            "status":todo.status,
-            "title":todo.title,
-            "image":todo?.image ? todo.image : null
-        })
-    })
+
     return groupedTodosByColumn
 }
