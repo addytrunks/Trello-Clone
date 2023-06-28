f484ed4748e69aea0bb5b65e36cee18e061c0aa6
Adhithya Srivatsan <addytrunks@gmail.com>
2023-06-28 15:41:55 +0530

    Drag and Drop Feature(Without Functionality) Implemented

--- components/Board.jsx
+++ components/Board.jsx
@@ -1,14 +1,49 @@
 'use client'
 
+import { useBoardStore } from '@/store/boardStore';
+import { useEffect } from 'react';
 import { DragDropContext,Droppable } from 'react-beautiful-dnd';
+import Column from './Column';
 
 const Board = () => {
+
+    const [board,getBoard] = useBoardStore((state) =>[state.board,state.getBoard])
+
+    useEffect(() => {
+        getBoard()
+    },[getBoard])
+
+    const handleDragEnd = (result) => {}
+
   return (
-    <DragDropContext>
+    <DragDropContext onDragEnd={handleDragEnd}>
         <Droppable droppableId='board' direction='horizontal' type='column'>
-            {(provided) => (
-                <div>
-                    {/* rendering all columns */}
+            {(provided,snapshot) => (
+                <div {...provided.droppableProps} ref={provided.innerRef} className='grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl'>
+                    <Column
+                    key={'todo'}
+                    id={'todo'}
+                    status={'todo'}
+                    todos={board["todo"]} 
+                    index={'1'}   
+                    />
+
+                    <Column
+                    key={'inprogress'}
+                    id={'inprogress'}
+                    status={'inprogress'}
+                    todos={board["inprogress"]} 
+                    index={'2'}   
+                    />
+
+                    <Column
+                    key={'done'}
+                    id={'done'}
+                    status={'done'}
+                    todos={board["done"]}  
+                    index={'3'}  
+                    />
+
                 </div>
             )}
         </Droppable>
