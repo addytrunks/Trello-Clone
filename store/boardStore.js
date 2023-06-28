08f05a35bc924bf0ec08da4fc2a88d92f9495ac8
Adhithya Srivatsan <addytrunks@gmail.com>
2023-06-28 19:05:41 +0530

    Drag and Drop Function Implemented/Update changes to DB Function Implemented

--- store/boardStore.js
+++ store/boardStore.js
@@ -1,3 +1,4 @@
+import { databases } from '@/appwrite'
 import { getTodosGroupedByColumn } from '@/utilities/getTodosGroupedByColumn'
 import { create } from 'zustand'
 
@@ -8,5 +9,12 @@ export const useBoardStore = create((set) => ({
   getBoard: async () => {
     const board = await getTodosGroupedByColumn()
     set({board:board})
+  },
+  setBoardState:(board)=>set({board}),
+  updateTodoinDB:async(todo,columnId) => {
+    await databases.updateDocument(process.env.NEXT_PUBLIC_DATABASE_ID,process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID,todo.$id,{
+      title:todo.title,
+      status:columnId
+    })
   }
 }))
